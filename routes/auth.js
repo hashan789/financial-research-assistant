6626c62c89967975aec7724293d803e2783c97ad
hashan789 <hashanmalind123@gmail.com>
2025-02-23 22:15:40 +0530

    removed send

--- routes/auth.js
+++ routes/auth.js
@@ -7,19 +7,19 @@ router.post("/", async (req,res) => {
     try{
         const { error } = validate(req.body);
         if (error)
-            return res.status(400).send({ message : error.details[0].message })
+            return res.status(400).json({ message : error.details[0].message })
 
         const user = await User.findOne({ email : req.body.email })
         if (!user)
-            return res.status(401).send({ message : "Invalid email or password" })
+            return res.status(401).json({ message : "Invalid email or password" })
 
         const validPassword = await bcrypt.compare(req.body.password, user.password);
         if (!validPassword){
-            return res.status(401).send({ message : "Invalid email or password" })
+            return res.status(401).json({ message : "Invalid email or password" })
         }
                    
         const token = user.generateAuthToken();
-        res.status(200).send({ data: token , message : "Logged in successfully" })
+        res.status(200).json({ data: token , message : "Logged in successfully" })
 
     }
     catch(error){
