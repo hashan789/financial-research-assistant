a23aea19ca999548ce9ca204c27cfa34e8b161a7
hashan789 <hashanmalind123@gmail.com>
2025-02-23 22:59:28 +0530

    removed credentials

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,8 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'https://hashsports-backend-bjdrg6deefc8fhcr.canadacentral-01.azurewebsites.net/api/',
-    withCredentials: true,
+    baseURL: 'https://hashsports-backend-bjdrg6deefc8fhcr.canadacentral-01.azurewebsites.net/api/'
 })
 
 export default axiosInstance;
